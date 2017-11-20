184e288575a0822244415ce86532df39338cea64
Athanasios Oikonomou <athoik@gmail.com>
2017-11-20 18:10:49 +0200

    Rename force DVB API 3 to force legacy stats
    Rename the setting name because is a bit misleading. It's only the signal strength and CNR readout which is forced to use the pre-5.10 ioctl.
    
    As suggested by @pieterg in https://github.com/OpenPLi/enigma2/commit/2bc59ac76f993bc5e2e0958d1acbc892b7e155b5#commitcomment-25726730

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1239,8 +1239,8 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 
 int eDVBFrontend::readFrontendData(int type)
 {
-	char force_dvb_api_3[64];
-	sprintf(force_dvb_api_3, "config.Nims.%d.force_dvb_api_3", m_slotid);
+	char force_legacy_signal_stats[64];
+	sprintf(force_legacy_signal_stats, "config.Nims.%d.force_legacy_signal_stats", m_slotid);
 
 	switch(type)
 	{
@@ -1275,7 +1275,7 @@ int eDVBFrontend::readFrontendData(int type)
 				int signalquality = 0;
 				int signalqualitydb = 0;
 #if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 10
-				if (m_dvbversion >= DVB_VERSION(5, 10) && !eConfigManager::getConfigBoolValue(force_dvb_api_3, false))
+				if (m_dvbversion >= DVB_VERSION(5, 10) && !eConfigManager::getConfigBoolValue(force_legacy_signal_stats, false))
 				{
 					dtv_property prop[1];
 					memset(prop, 0, sizeof(prop));
@@ -1338,7 +1338,7 @@ int eDVBFrontend::readFrontendData(int type)
 				if (!m_simulate)
 				{
 #if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 10
-					if (m_dvbversion >= DVB_VERSION(5, 10) && !eConfigManager::getConfigBoolValue(force_dvb_api_3, false))
+					if (m_dvbversion >= DVB_VERSION(5, 10) && !eConfigManager::getConfigBoolValue(force_legacy_signal_stats, false))
 					{
 						dtv_property prop[1];
 						memset(prop, 0, sizeof(prop));
